ca444267e419f761aa956c2477114271cf3ab85c
harpreet-1 <hpsllg8@gmail.com>
2022-11-30 14:59:47 +0530

    palindrome test

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+let str="madam"
+let r="No"
+for(i=0;i<str.length/2;i++){
+	if(str[i]==str[str.length-1-i]){
+            r="yes";
+        }
+    else{
+        r="yes"
+    }
+ }       
+ console.log(r)
